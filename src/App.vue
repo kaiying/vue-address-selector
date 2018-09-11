925da984fc0622e9ea35fd7b569e113c785c24a2
KathyLai <kathy.lai@senao.com.tw>
2018-09-11 18:00:58 +0800

    Vue Day 4 homework

--- src/App.vue
+++ src/App.vue
@@ -17,104 +17,9 @@
 
 <script>
 import MySelector from './components/MySelector.vue';
+import { API } from './env.js';
+import axios from 'axios';
 
-const cities = [
-  {
-    name: '基隆市',
-    areas: [
-      {
-        name: '仁愛區',
-        zip: '200',
-      },
-      {
-        name: '信義區',
-        zip: '201',
-      },
-      {
-        name: '中正區',
-        zip: '202',
-      },
-      {
-        name: '中山區',
-        zip: '203',
-      },
-      {
-        name: '安樂區',
-        zip: '204',
-      },
-      {
-        name: '暖暖區',
-        zip: '205',
-      },
-      {
-        name: '七堵區',
-        zip: '206',
-      },
-    ],
-  },
-  {
-    name: '台北市',
-    areas: [
-      {
-        name: '中正區',
-        zip: '300',
-      },
-      {
-        name: '大同區',
-        zip: '301',
-      },
-      {
-        name: '中山區',
-        zip: '302',
-      },
-      {
-        name: '松山區',
-        zip: '303',
-      },
-      {
-        name: '大安區',
-        zip: '304',
-      },
-      {
-        name: '萬華區',
-        zip: '305',
-      },
-      {
-        name: '信義區',
-        zip: '306',
-      },
-      {
-        name: '士林區',
-        zip: '307',
-      },
-      {
-        name: '北投區',
-        zip: '308',
-      },
-      {
-        name: '內湖區',
-        zip: '309',
-      },
-      {
-        name: '南港區',
-        zip: '310',
-      },
-      {
-        name: '文山區',
-        zip: '311',
-      },
-    ],
-  },
-  {
-    name: '新竹市',
-    areas: [
-      {
-        name: '新竹市',
-        zip: '400',
-      },
-    ],
-  },
-];
 
 export default {
   name: 'app',
@@ -125,17 +30,50 @@ export default {
     return {
       city: 0,
       district: 0,
-      cities,
+      cities: [
+        {
+          name: '',
+          areas: [
+            {
+              name: '',
+              zip: '',
+            },
+          ],
+        }],
       zip: '',
+      url: API,
+      error: '',
+      isLoad: false,
     };
+  },
+  created() {
+    console.log('ajax 底加拉');
+    console.log(this.url);
+
+    const response = (res) => {
+      console.log(res.data);
+      //
+      if (res.status === 200) {
+        this.cities = res.data;
+      } else {
+        this.error = 'data source';
+      }
+    };
+
+    const error = e => console.log(e);
+
+    axios
+      .post(this.url)
+      .then(response)
+      .catch(error);
   },
   computed: {
     sourceCities() {
       this.getZip(0);
-      return cities;
+      return this.cities;
     },
     sourceDist() {
-      return cities[this.city].areas;
+      return this.cities[this.city].areas;
     },
   },
   methods: {
@@ -147,7 +85,7 @@ export default {
       this.getZip(value);
     },
     getZip(value) {
-      this.zip = cities[this.city].areas[value].zip;
+      this.zip = this.cities[this.city].areas[value].zip;
     },
   },
   watch: {
